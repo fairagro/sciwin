dcb02adf0f656c6285dbd05a0653e3f3f9985193
Jens Krumsieck <j.krumsieck@outlook.de>
2026-05-18 16:25:03 +0200

    remove old cwl packages

--- packages/cwl_core/src/prelude.rs
+++
@@ -1,9 +0,0 @@
-pub use crate::{
-    Argument, CWLDocument, CWLType, Command, CommandLineTool, Expression, ExpressionTool, ExpressionType, Workflow, WorkflowStep,
-    inputs::{CommandInputParameter, CommandLineBinding, WorkflowStepInputParameter},
-    outputs::{CommandOutputBinding, CommandOutputParameter, WorkflowOutputParameter},
-    requirements::{
-        DockerRequirement, EnvVarRequirement, InitialWorkDirRequirement, InlineJavascriptRequirement, NetworkAccess, Requirement, ToolTimeLimit,
-    },
-    types::{DefaultValue, Directory, Dirent, File},
-};
