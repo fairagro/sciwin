d44a780b621453d77910fe2d55a2789a20aafdf4
Jens Krumsieck <j.krumsieck@outlook.de>
2026-08-21 06:56:07 +0000

    canonicalize dir

--- crates/sciwin/container/mod.rs
+++ crates/sciwin/container/mod.rs
@@ -9,10 +9,11 @@ pub mod r;
 pub mod resolver;
 
 pub async fn resolve_python_container(working_dir: &Path) -> AuthoringResult<Option<Image>> {
+    let working_dir = dunce::canonicalize(working_dir)?;
     let dependencies = if working_dir.join("requirements.txt").exists() {
         python::requirements_from_requirements_txt(
             &working_dir.join("requirements.txt"),
-            working_dir,
+            &working_dir,
         )
         .await?
     } else if working_dir.join("pyproject.toml").exists() {
@@ -32,6 +33,7 @@ pub async fn resolve_python_container(working_dir: &Path) -> AuthoringResult<Opt
 }
 
 pub async fn resolve_r_container(working_dir: &Path) -> AuthoringResult<Option<Image>> {
+    let working_dir = dunce::canonicalize(working_dir)?;
     let dependencies = if working_dir.join("DESCRIPTION").exists() {
         r::requirements_from_description(&working_dir.join("DESCRIPTION")).await?
     } else {
